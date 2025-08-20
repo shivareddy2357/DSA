c4006f1dae1f4e9936a2924fe87a2fbf5d06c6b6
shiva reddy Nimmala <71578970+shivareddy2357@users.noreply.github.com>
2025-08-20 21:52:38 +0530

    Time: 1 ms (84.34%) | Memory: 40.4 MB (56.28%) - LeetSync

--- 755-reach-a-number/reach-a-number.java
+++ 755-reach-a-number/reach-a-number.java
@@ -1,17 +1,14 @@
 class Solution {
     public int reachNumber(int target) {
         int sum =0 ,steps = 0;
-        int a = target;
-        if(a ==0){
-             return 0;
-        }
-        a = Math.abs(a);
-        while(sum< a){
+        if(target ==0) return 0;
+        target = Math.abs(target);
+        while(sum< target){
             sum+=steps;
             steps++;
         }
         
-        while(((sum-a)%2!=0)){
+        while(((sum-target)%2!=0)){
             sum+=steps;
             steps++;
         }
