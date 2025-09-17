b03686a2e5b139cf6cb8e1bc72c926c1108f90ec
shiva reddy Nimmala <71578970+shivareddy2357@users.noreply.github.com>
2025-09-17 22:28:23 +0530

    Time: 2 ms (88.27%) | Memory: 46.6 MB (5.16%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -12,25 +12,39 @@ class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists==null || lists.length==0) return null;
         
-        ListNode head=new ListNode(0);
-        ListNode temp=head;
+        int interval=1;
+        while(interval<lists.length){
+            for(int i=0;i+interval<lists.length;i=i+interval*2){
+                lists[i]=mergeTwoLists(lists[i],lists[i+interval]);
+            }
+            interval*=2;
+        }
+        return lists[0];
+    }
+    
+    ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null) return l2;
+        if(l2==null) return l1;
         
-        while(true){
-            int p=0;
-            for(int i=0;i<lists.length;i++){
-                if(lists[p]==null || (lists[i]!=null && lists[p].val>lists[i].val)){
-                    p=i;
-                }
+        ListNode newHead=new ListNode(l1.val);
+        ListNode temp=newHead;
+        while(l2!=null && l1!=null){
+            if(l1.val<=l2.val){
+                temp.next=new ListNode(l1.val);
+                l1=l1.next;
+            }else{
+                temp.next=new ListNode(l2.val);
+                l2=l2.next;
             }
-            
-            if(lists[p]==null){
-                break;
-            }
-            temp.next=new ListNode(lists[p].val);
             temp=temp.next;
-            lists[p]=lists[p].next;
         }
         
-        return head.next;
+        if(l1!=null){
+            temp.next=l1;
+        } 
+        else{
+            temp.next=l2;
+        }
+        return newHead.next;
     }
 }
