fe1551e78440e78bb9a627b632b95c5d394211c5
shiva reddy Nimmala <71578970+shivareddy2357@users.noreply.github.com>
2025-09-18 22:59:07 +0530

    Time: 65 ms (82.09%) | Memory: 46.2 MB (76.71%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,35 +1,38 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashSet<String> set = new HashSet<>();
-        for(String x : wordList){
-            set.add(x);
-        }
-        set.remove(beginWord);
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(beginWord,1));
+        HashSet<String> set=new HashSet<>(wordList);
+        if(!set.contains(endWord)) return 0;
+        Queue<String> q=new LinkedList<>();
+        q.offer(beginWord);
+        if(set.contains(beginWord)) set.remove(beginWord);
+        int level=0;
         while(!q.isEmpty()){
-            int steps = q.peek().steps;
-            String word = q.peek().word;
-            q.remove();
-            if(word.equals(endWord)) return steps;
-            for(int i = 0; i < word.length(); i++){
-                for(char ch = 'a'; ch <= 'z'; ch++){
-                    String newString = word.substring(0,i) + ch + word.substring(i+1);
-                    if(set.contains(newString)){
-                        set.remove(newString);
-                        q.offer(new Pair(newString,steps+1));
-                    }
+            int n=q.size();
+            for(int el=0;el<n;el++){
+                String node=q.poll();
+                if(node.equals(endWord)) return level+1;
+                List<String> Node_list=neighbours(node,set);
+                for(String s:Node_list){
+                    q.offer(s);
+                    set.remove(s);
                 }
             }
+            level++;
         }
         return 0;
     }
-}
-class Pair{
-    int steps;
-    String word;
-    Pair(String word, int steps){
-        this.steps = steps;
-        this.word = word;
+    public List<String> neighbours(String node,HashSet<String> set){
+        List<String> list=new ArrayList<>();
+        char[] node_chars=node.toCharArray();
+        for(int i=0;i<node_chars.length;i++){
+            char original=node_chars[i];
+            for(char ch='a';ch<='z';ch++){
+                node_chars[i]=ch;
+                String new_word=String.valueOf(node_chars);
+                if(set.contains(new_word)) list.add(new_word);
+            }
+            node_chars[i]=original;
+        }
+        return list;
     }
 }
